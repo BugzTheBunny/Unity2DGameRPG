046248a17c1ccdcfe3922d2c1b69b31be411f4c1
Slava <urigami2010@gmail.com>
2024-03-12 06:51:49 +0200

    state machine started

---
+++ Assets/Scripts/Player/States/PlayerMoveState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveState : PlayerState
+{
+    public PlayerMoveState(Player _player, PlayerStateMachine _stateMachine, string animBoolName) : base(_player, _stateMachine, animBoolName)
+    {
+    }
+}
